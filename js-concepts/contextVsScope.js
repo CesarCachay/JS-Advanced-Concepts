861077f93322cb66eed813e9d858a3214b3a2b61
CesarCachay <cesarcachay1996@gmail.com>
2020-02-26 09:51:13 -0500

    Add section review of js concepts

---
+++ js-concepts/contextVsScope.js
@@ -0,0 +1,13 @@
+// -------------------------- SCOPE -------------------------- //
+// - Scope is a function based thing.
+// - Scope means what is the variable access of a function when it is invoked.
+// - What is the variable environment.
+
+// -------------------------- CONTEXT -------------------------- //
+// - Context is more about object based.
+// - It refers what is the value of the "THIS" keyword.
+// - Which is a reference to the object THAT OWNS, that current executing code.
+
+// ---------------------  TO KEEP IN MIND ---------------------- //
+// Context is most often determined by how a function is invoked with the value of "THIS" keyword.
+// Scope refers to the visibility of variables.
---
+++ js-concepts/sectionReview.js
@@ -0,0 +1,20 @@
+const foundations = {
+  executionContext:
+    "Refers how every function invocation creates an execution context",
+  lexicalEnvironment:
+    "That term is related on where we write our code is important ",
+  scopeChain:
+    "Is a way for us to look for variables and see what we have access to",
+  hoisting:
+    "This term is related to how JavaScript does it's weird thing that hoists function declaration and variable declartions",
+  functionInvocation:
+    "Refers to interesting properties when we create new functions such as getting the arguments parameter or modify the 'THIS' keyword",
+  fnScopeVsBlockScope:
+    "Related to how JavaScript originally only had function scope but when ES6 arrived with 'CONST' & 'LET' we can use block scope",
+  dynamicVsLexicalScope:
+    "We learned that JavaScript just has lexical scope with the exception of the 'THIS' keyword which has dynamically scoping",
+  thisCallApplyBind:
+    "The benefits of those important keyword for senior software developer engineers",
+  immediatelyInvokedFunctionExpression: 
+    "(IIFE) "
+};
